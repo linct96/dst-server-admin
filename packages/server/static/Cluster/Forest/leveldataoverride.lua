dfe7b3dd25f67993bac9800256b33e5ebfa1c6cd
linct96 <linct96@github.com>
2024-07-08 10:54:03 +0800

    wip

---
+++ packages/server/static/Cluster/Forest/leveldataoverride.lua
@@ -0,0 +1,26 @@
+return {
+  id="SURVIVAL_TOGETHER",
+  name="标准森林",
+  desc="标准《饥荒》体验。",
+  hideminimap=false,
+  location="forest",
+  max_playlist_position=999,
+  min_playlist_position=0,
+  numrandom_set_pieces=4,
+  override_level_string=false,
+  overrides={
+    world_size="default",
+  },
+  random_set_pieces={,
+  },
+  required_prefabs={ "multiplayer_portal" },
+  required_setpieces={ "Sculptures_1", "Maxwell5" },
+  settings_desc="标准《饥荒》体验。",
+  settings_id="SURVIVAL_TOGETHER,"
+  settings_name="标准森林",
+  substitutes={  },
+  version=4,
+  worldgen_desc="标准《饥荒》体验。",
+  worldgen_id="SURVIVAL_TOGETHER",
+  worldgen_name="标准森林" 
+}
