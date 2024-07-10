8756b147565a143c49dc47afe14b20a833d3fac9
linct96 <linct96@outlook.com>
2024-07-11 00:09:21 +0800

    wip

--- packages/server/src/ssh.ts
+++ packages/server/src/ssh.ts
@@ -1,4 +1,6 @@
 import { NodeSSH } from 'node-ssh'
+import { homedir } from 'node:os'
+import { resolve } from 'node:path'
 
 const ssh = new NodeSSH()
 
@@ -6,10 +8,12 @@ export const getSSH = async () => {
   if (ssh.isConnected()) {
     return ssh
   } else {
+    console.log('connecting to ssh', homedir())
     await ssh.connect({
       host: '101.126.78.130',
       username: 'root',
-      privateKeyPath: '/Users/linchaoting/.ssh/byte_id_rsa'
+      // privateKeyPath: '/Users/linchaoting/.ssh/byte_id_rsa'
+      privateKeyPath: resolve(homedir(), '.ssh', 'byte_id_rsa')
     })
   }
   return ssh
