e05ebc6cf075e338243cce012a29ca3d79595410
linct96 <linct96@outlook.com>
2024-11-19 23:05:05 +0800

    wip

--- server/src/service/task.rs
+++ server/src/service/task.rs
@@ -1,13 +1,10 @@
 use std::sync::Arc;
 
 use once_cell::sync::Lazy;
-use psutil::{cpu, disk, memory};
 
+use rust_decimal::prelude::ToPrimitive;
 use serde::Serialize;
-use tokio::{
-    sync::Mutex,
-    time::{self, Duration},
-};
+use tokio::sync::Mutex;
 
 #[derive(Debug, Serialize, Clone, Default)]
 pub struct SystemInfo {
@@ -29,28 +26,41 @@ pub static SYSTEM_INFO: Lazy<Arc<Mutex<SystemInfo>>> = Lazy::new(|| {
 
 pub async fn update_system_info() {
     // 创建一个新的 System 实例
+    let mut sys = sysinfo::System::new_all();
+    let disks = sysinfo::Disks::new_with_refreshed_list();
+    sys.refresh_all();
+    tokio::time::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL).await;
+
     let mut system_info = SYSTEM_INFO.lock().await;
-    let mut cpu_collector = cpu::CpuPercentCollector::new().unwrap();
-    let disk_info = disk::disk_usage("/").unwrap();
-    cpu_collector
-        .cpu_percent()
-        .expect("Failed to get CPU usage");
-    time::sleep(Duration::from_secs(1)).await;
-    let memory_info = memory::virtual_memory().unwrap();
+    // let mut cpu_collector = cpu::CpuPercentCollector::new().unwrap();
+    // let disk_info = disk::disk_usage("/").unwrap();
+    // cpu_collector
+    //     .cpu_percent()
+    //     .expect("Failed to get CPU usage");
+    // time::sleep(Duration::from_secs(1)).await;
+    // let memory_info = memory::virtual_memory().unwrap();
+    let mut disk_total: u64 = 0; // 用于累加总大小
+    let mut disk_used: u64 = 0; // 用于累加已使用大小
+    disks.iter().for_each(|disk| {
+        disk_total += disk.total_space();
+        disk_used += disk.available_space();
+    });
+    let cpu_usage_count: f32 = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).sum();
     system_info.os_version =
         sysinfo::System::long_os_version().unwrap_or_else(|| "unknown".to_owned());
-    system_info.cpu_count = cpu::cpu_count();
-    system_info.cpu_usage = (cpu_collector.cpu_percent().unwrap() * 10.0).round() / 10.0;
-    system_info.disk_used =
-        (disk_info.used() as f32 / (1024 * 1024 * 1024) as f32 * 10.0).round() / 10.0;
+    system_info.cpu_count = sys.cpus().len().to_u64().unwrap();
+    system_info.cpu_usage = (10.0 * cpu_usage_count / sys.cpus().len() as f32).round() / 10.0;
+    system_info.disk_used = (10.0 * disk_used as f32 / (1024 * 1024 * 1024) as f32).round() / 10.0;
     system_info.disk_total =
-        (disk_info.total() as f32 / (1024 * 1024 * 1024) as f32 * 10.0).round() / 10.0;
-    system_info.disk_usage = (disk_info.percent() * 10.0).round() / 10.0;
+        (10.0 * disk_total as f32 / (1024 * 1024 * 1024) as f32).round() / 10.0;
+    system_info.disk_usage =
+        (system_info.disk_used / system_info.disk_total * 1000.0).round() / 10.0;
     system_info.memory_total =
-        (memory_info.total() as f32 / (1024 * 1024 * 1024) as f32 * 10.0).round() / 10.0;
+        (sys.total_memory() as f32 / (1024 * 1024 * 1024) as f32 * 10.0).round() / 10.0;
     system_info.memory_used =
-        (memory_info.used() as f32 / (1024 * 1024 * 1024) as f32 * 10.0).round() / 10.0;
-    system_info.memory_usage = (memory_info.percent() * 10.0).round() / 10.0;
+        (sys.used_memory() as f32 / (1024 * 1024 * 1024) as f32 * 10.0).round() / 10.0;
+    system_info.memory_usage =
+        (system_info.memory_used / system_info.memory_total * 1000.0).round() / 10.0;
 }
 
 pub async fn get_system_info() -> SystemInfo {
